11379af951e343fbf435856346705dc8a84de0d7
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-19 19:30:51 +0100

    Add DataBase.record_file_log

--- factlog/database.py
+++ factlog/database.py
@@ -27,3 +27,25 @@ class DataBase(object):
                 'insert into system_info (version) values (?)',
                 [version])
             db.commit()
+
+    def record_file_log(self, file_path, activity_type, file_point=None):
+        """
+        Record file activity.
+
+        :type     file_path: str
+        :arg      file_path: path to the file
+        :type    file_point: int or None
+        :arg     file_point: point of cursor at the time of saving.
+        :type activity_type: str
+        :arg  activity_type: one of 'write', 'open', 'close'
+
+        """
+        assert activity_type in ('write', 'open', 'close')
+        with closing(self._get_db()) as db:
+            db.execute(
+                """
+                insert into system_info (file_path, file_point, activity_type)
+                values (?, ?, ?)
+                """,
+                [file_path, file_point, activity_type])
+            db.commit()
--- factlog/schema.sql
+++ factlog/schema.sql
@@ -2,6 +2,7 @@ drop table if exists file_log;
 create table file_log (
   id integer primary key autoincrement,
   file_path string not null,
+  file_point integer,
   recorded timestamp default current_timestamp,
   activity_type string not null
 );
