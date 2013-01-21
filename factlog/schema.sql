e1211bfdf93f93733a6a790db8e65452b4c63be5
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-21 14:39:11 +0100

    Add more columns to table

--- factlog/schema.sql
+++ factlog/schema.sql
@@ -3,12 +3,14 @@ create table file_log (
   id integer primary key autoincrement,
   file_path string not null,
   file_point integer,
+  file_exists integer not null default 1,
   recorded timestamp default current_timestamp,
   activity_type string not null
 );
 
 drop table if exists system_info;
 create table system_info (
-  version string primary key,
+  factlog_version string not null,
+  schema_version string not null,
   updated timestamp default current_timestamp
 );
