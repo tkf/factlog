dc08c5f1543375a2a74427dd9c21aede59826323
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-24 15:15:26 +0100

    Add doctest to remove_prefix

--- factlog/utils/strutils.py
+++ factlog/utils/strutils.py
@@ -1,6 +1,12 @@
 def remove_prefix(prefixes, string):
     """
     Remove prefix of string if one of the candidate in `prefixes` matches.
+
+    >>> remove_prefix(['aaa', 'aa', 'a'], 'aaax')
+    'x'
+    >>> remove_prefix(['aaa', 'aa', 'a'], 'aax')
+    'x'
+
     """
     for pre in prefixes:
         if string.startswith(pre):
