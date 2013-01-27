fe6675455ab0619ce3085340ba727556df40019f
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-27 14:57:39 +0100

    Install factlog CLI

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 import factlog
 
@@ -20,4 +20,7 @@ setup(
     install_requires=[
         'argparse',
     ],
+    entry_points={
+        'console_scripts': ['factlog = factlog.cli:main'],
+    },
 )
