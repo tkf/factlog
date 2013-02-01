93927b7af20e7b0ccdb72932f0deb2f955bf2bb8
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-01 16:26:13 +0100

    Use TEXT data type instead of STRING in schema.sql
    %s/string/text/g

--- factlog/schema.sql
+++ factlog/schema.sql
@@ -1,17 +1,17 @@
 drop table if exists access_log;
 create table access_log (
   id integer primary key autoincrement,
-  file_path string not null,
+  file_path text not null,
   file_point integer,
   file_exists integer not null default 1,
-  program string,
+  program text,
   recorded timestamp default current_timestamp,
   access_type integer not null
 );
 
 drop table if exists factlog_info;
 create table factlog_info (
-  factlog_version string not null,
-  schema_version string not null,
+  factlog_version text not null,
+  schema_version text not null,
   updated timestamp default current_timestamp
 );
